7f5e7de6ef673b0f16912e0080b38691d38627ad
Zehan Khan <zehan9211@gmail.com>
2022-07-05 00:49:40 +0530

    EX:1.1.5 objects

--- Part-1/course/src/App.js
+++ Part-1/course/src/App.js
@@ -17,37 +17,41 @@ const Example = () => {
 
 const App = () => {
   // const-definitions
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
   return (
     <div>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total part1={part1} part2={part2} part3={part3} />
+      <Header course={course.name} />
+      <Content part={course.parts} />
+      <Total exercise={course.parts} />
     </div>
   )
 }
 
 const Header = ( props ) => <h1>{props.course}</h1>
 
-const Total = ( props ) => <p>Number of exercise { props.part1.exercises + props.part2.exercises + props.part2.exercises }</p>
+const Total = ( props ) => <p>Number of exercise { props.exercise.reduce((acc,cv)=>acc+=cv.exercises,0) }</p>
 
 const Content = ( props ) => {
   return <div>
-    <Parts name={props.part1.name} exercise={props.part1.exercises}  />
-    <Parts name={props.part2.name} exercise={props.part2.exercises}  />
-    <Parts name={props.part3.name} exercise={props.part3.exercises}  />
+    <Parts name={props.part[0].name} exercise={props.part[0].exercises} />
+    <Parts name={props.part[1].name} exercise={props.part[1].exercises} />
+    <Parts name={props.part[2].name} exercise={props.part[2].exercises} />
   </div>
 }
 
