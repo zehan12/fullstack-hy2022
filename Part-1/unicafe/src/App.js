ab5cfa8cbfe255f63d1d562ad35935356ed3a41f
Zehan Khan <zehan9211@gmail.com>
2022-08-20 15:17:31 +0530

    EX:1.9  unicafe step 4

--- Part-1/unicafe/src/App.js
+++ Part-1/unicafe/src/App.js
@@ -5,24 +5,26 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const total = good + neutral + bad;
 
   return (
     <div>
       <h2>give feedback</h2>
-      <div style={{display:"flex"}}>
-      <button onClick={()=>setGood(good+1)} >good</button>
-      <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
-      <button onClick={()=>setBad(bad+1)}>bad</button>
+      <div style={{ display: "flex" }}>
+        <button onClick={() => setGood(good + 1)} >good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
       </div>
 
       <h2>statics</h2>
-      <Statistics text={"good"} val={good} />
-      <Statistics text={"bad"} val={bad} />
-      <Statistics text={"neutral"} val={neutral} />
-      <p>all: { total }</p>
-      <p>avarage: { good + 0 - bad / total || 0 }</p>
-      <p>percentage { good / total * 100 || 0} </p>
+      {
+        (good || neutral || bad) ?
+          <>
+            <Statistics text={"good"} val={good} />
+            <Statistics text={"bad"} val={bad} />
+            <Statistics text={"neutral"} val={neutral} />
+          </>
+          : <p>No feedback given</p>
+      }
     </div>
   )
 }
@@ -30,7 +32,7 @@ const App = () => {
 const Statistics = ({ text, val }) => {
   // ...
   return <div>
-      <p> {text}: {val}</p>
+    <p> {text}: {val}</p>
   </div>
 }
 
