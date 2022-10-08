5b8c11fbab762a023ff945e6cd2f67b15d0395d9
Zehan Khan <zehan9211@gmail.com>
2022-10-09 02:45:27 +0530

    Excercise 2.4 : Course information  step9

--- Part-2/notes/src/App.js
+++ Part-2/notes/src/App.js
@@ -33,29 +33,53 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
+    const courses = [
       {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
+        name: 'Half Stack application development',
+        id: 1,
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10,
+            id: 1
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7,
+            id: 2
+          },
+          {
+            name: 'State of a component',
+            exercises: 14,
+            id: 3
+          },
+          {
+            name: 'Redux',
+            exercises: 11,
+            id: 4
+          }
+        ]
+      }, 
       {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
+        name: 'Node.js',
+        id: 2,
+        parts: [
+          {
+            name: 'Routing',
+            exercises: 3,
+            id: 1
+          },
+          {
+            name: 'Middlewares',
+            exercises: 7,
+            id: 2
+          }
+        ]
       }
     ]
-  }
 
-  return <Course course={course} />
+  return courses.map((course)=> <Course course={course} /> )
+
 }
 
 export default App
