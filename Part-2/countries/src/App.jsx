c103b16999a359e2a150d5cbdf9437a5b97b1bc2
Zehan Khan <zehan9211@gmail.com>
2022-12-20 17:45:57 +0530

    Excercise 2.13* couuntries step2

--- Part-2/countries/src/App.jsx
+++ Part-2/countries/src/App.jsx
@@ -6,41 +6,59 @@ import { useEffect } from 'react';
 function App() {
   const [countries, setCountries] = useState([]);
   const [query, setQuery] = useState(null);
-  const [display, setDisplay] = useState([])
+  const [display, setDisplay] = useState("")
   const [result, setResult] = useState([])
 
   const findCountry = (e) => {
     setQuery(e.target.value)
     const result = countries.filter((country) => country?.name?.common?.split(" ")[0].toLowerCase().includes(query.toLowerCase()));
-    if ( result.length !== 1 ) return setResult(result);
-    if ( result.length === 1 ) return setDisplay(result);
+    if (result.length !== 1 && result.length > 4) return setResult(result.splice(0, 4));
+    if (result.length !== 1) return setResult(result)
+    if (result.length === 1) return setDisplay(result[0]);
   }
+
+  const handleClick = (country) => {
+    console.log("clicked")
+    setDisplay(country)
+  }
+
 
   const getAllCountries = async () => {
     const { data } = await axios.get("https://restcountries.com/v3.1/all")
-
     setCountries(data.flat(Infinity))
-    console.log(countries)
   }
 
   useEffect(() => {
     getAllCountries()
   }, [])
 
-  console.log(display,"di",display[name])
+  console.log(display)
   return (
     <div className="App" >
       <input type="text" value={query} name="query" onChange={(e) => findCountry(e)} />
       {
         result.length &&
-        result.map((country) => <h2>{country.name.common}</h2>)
+        result.map((country) => (
+          <div style={{ display: "flex", alignItems: "center", gap: "10px" }} >
+            <h3>{country.name.common}</h3>
+            <button style={{ height: "20px" }} onClick={() => handleClick(country)}>show</button>
+          </div>)
+        )
       }
       {
-         display.length && 
-         <>
-         <h1>{display[0].name.common}</h1>
-         <img src={display[0].flags.png} />
-         </>
+        display &&
+        <>
+          <h1>{display.name.common}</h1>
+          <img src={display.flags.png} />
+          <p>continent: {display.continents[0]}</p>
+          <p>capital:{display.capital[0]}</p>
+          <p>population: {display.population}</p>
+          <h5>Languages</h5>
+          {
+           Object.entries(display.languages).map(((e, i) => <li>{e[i]}</li>))
+          }
+
+        </>
 
       }
     </div>
