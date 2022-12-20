c57d75cce93023deaa8340b859b8d8ca01fcc3c6
Zehan Khan <zehan9211@gmail.com>
2022-12-20 15:02:56 +0530

    Excercise 2.9*  phonebook step4

--- Part-2/phonebook/src/App.js
+++ Part-2/phonebook/src/App.js
@@ -1,7 +1,12 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-123456' }])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
 
