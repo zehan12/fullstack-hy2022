15aa01fb5fa2f33a343d21a885d9dd1895a4eccb
Zehan Khan <zehan9211@gmail.com>
2022-12-20 14:55:04 +0530

    Excercise 2.7  phonebook step2

--- Part-2/phonebook/src/App.js
+++ Part-2/phonebook/src/App.js
@@ -1,23 +1,38 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
   const [newName, setNewName] = useState('')
+  const handleChange = ({ target }) => {
+    let { name, value } = target;
+    setNewName(value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(newName, persons)
+    if (persons.map((v) => v.name.toLowerCase()).includes(newName.toLowerCase())) {
+       alert(`${newName} is already added to phonebook`)
+       return
+    }
+    setPersons([...persons, { name: newName }]);
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input type="name" value={newName} onChange={(e) => handleChange(e)} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button onClick={(e) => handleSubmit(e)} type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      {
+        persons.map((person) => <h2>{person.name}</h2>)
+      }
       ...
     </div>
   )
